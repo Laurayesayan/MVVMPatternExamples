99ffd97d6256b88a4e7171eba1ef5cf0198b44ce
laurayesayan <laurayesayan@gmail.com>
2020-02-26 22:09:46 +0300

    Initial Commit

---
+++ ArchitectureExamples_CategoriesLoader_2_4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArchitectureExamples_CategoriesLoader_2_4
+//
+//  Created by Лаура Есаян on 26.02.2020.
+//  Copyright © 2020 LY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
